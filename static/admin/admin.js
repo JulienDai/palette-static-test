f4bcabab985e069432ffcfb26f11982a7d30a003
Julien-DAI <julien.dai@imt-atlantique.net>
2024-01-17 09:31:49 +0100

    h

---
+++ static/admin/admin.js
@@ -0,0 +1,7 @@
+import CMS from '@staticcms/core';
+import '@staticcms/core/dist/main.css';
+
+// Initialize Static CMS object
+CMS.init();
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate('my-template', MyTemplate);
